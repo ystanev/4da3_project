a2cb960c9834c18ac72223d23a7dbbd1198b3b76
Yury Stanev <ystanev@yahoo.ca>
2019-04-29 13:27:06 -0400

    misc.

--- spam_linear_svm.py
+++ spam_linear_svm.py
@@ -1,20 +1,21 @@
 import time
 from numpy import *
-from sklearn.svm import LinearSVC  
+from sklearn.svm import LinearSVC
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
 
-#have to use default delimiter 'white space'
+# have to use default delimiter 'white space'
 rawTrainingData = loadtxt('spam.txt')  # read '.txt' file
 clf = LinearSVC()  # classifier
 
 X = rawTrainingData[:, 0:57]  # attributes , [raw, column]  from:to
 y = rawTrainingData[:, 57]  # class
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)  # spliting into test and training
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.25)  # spliting into test and training
 
-#Timing The Code
-start_time = time.time()  
+# Timing The Code
+start_time = time.time()
 
 clf.fit(X_train, y_train)  # fitting the model
 y_pred = clf.predict(X_test)  # predicted class
@@ -28,4 +29,4 @@ print (confusion_matrix(y_true, y_pred))
 print("\n", "Elapsed Time = ", elapsed_time*1000, " ms.")
 print("Accuracy: ", accuracy*100, "%")
 
-#print (y_train)
+# print ("\n", y_pred)
